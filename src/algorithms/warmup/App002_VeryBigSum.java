474e635f70ad7f6afb3ee5561859d0f63dfafd57
Zenceras <aiona243@gmail.com>
2018-09-08 17:05:00 +0300

    Update App002_VeryBigSum.java - added Method main()

--- src/algorithms/warmup/App002_VeryBigSum.java
+++ src/algorithms/warmup/App002_VeryBigSum.java
@@ -3,15 +3,13 @@ package algorithms.warmup; /**
  *
  * Created by vlad on 31.08.17.
  */
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
 
 public class App002_VeryBigSum {
+
+    public static void main(String[] args) {
+        System.out.println(aVeryBigSum(new long[] {1000000001, 1000000002,
+                1000000003, 1000000004, 1000000005})); //Expected output: 5000000015
+    }
 
     // Complete the aVeryBigSum function below.
     static long aVeryBigSum(long[] ar) {
